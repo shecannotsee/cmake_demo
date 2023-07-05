f07919eede19c7bdbb9cf8c5a164c1588a4170f2
shecannotsee <shecannotsee@163.com>
2023-07-05 18:00:13 +0800

    [feat] Using functions to handle repetitive processes

--- cmake/02_build_3rd.cmake
+++ cmake/02_build_3rd.cmake
@@ -1,18 +1,24 @@
 
 include(ExternalProject)
-if (NOT EXISTS "${CMAKE_SOURCE_DIR}/third_party/googletest-install")
-    message(STATUS "Need to compile third-party libraries:googletest.")
-    # compliling
-    ExternalProject_Add(
-            googletest
-            # 指定源码
-            SOURCE_DIR "${CMAKE_SOURCE_DIR}/third_party/googletest"
-            # 指定cmake的安装目录
-            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/third_party/googletest-install
-    )
-else()
-    message(STATUS "googletest does not need to be compiled again.")
-endif()
+
+function(compiling_3rd 3rd_name)
+    if (NOT EXISTS "${CMAKE_SOURCE_DIR}/third_party/${3rd_name}-install")
+        message(STATUS "Need to compile third-party libraries:${3rd_name}.")
+        # compliling
+        ExternalProject_Add(
+                googletest
+                # 指定源码
+                SOURCE_DIR "${CMAKE_SOURCE_DIR}/third_party/${3rd_name}"
+                # 指定cmake的安装目录
+                CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/third_party/${3rd_name}-install
+        )
+    else()
+        message(STATUS "${3rd_name} does not need to be compiled again.")
+    endif()
+endfunction()
+
+compiling_3rd(googletest)
+
 
 
 

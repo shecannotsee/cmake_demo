5df1e8c4f5b789558d693c1e8c51151401f4f928
shecannotsee <shecannotsee@163.com>
2023-07-05 11:44:58 +0800

    [feat] Add dependencies to build order

--- cmake/04_src.cmake
+++ cmake/04_src.cmake
@@ -5,13 +5,13 @@ file(GLOB_RECURSE SRC "${CMAKE_SOURCE_DIR}/src/*")
 # ld lib
 set(lib "-pthread"
         "-lgtest"
-        "-lgmock"
-        "-lglog")
+        "-lgmock")
 
 
 # exe target
 if (project_type STREQUAL "executable")
     add_executable(${project_name} ${SRC})
+    add_dependencies(${project_name} googletest)
     target_link_libraries(${project_name} ${lib})
     message(STATUS "This is an executable program")
 else()
@@ -21,6 +21,7 @@ endif ()
 # lib target
 if (project_type STREQUAL "library")
     add_library(${library_static_name} STATIC ${SRC})
+    add_dependencies(${library_static_name} googletest)
     # Rename the generated static library
     set_target_properties(${library_static_name} PROPERTIES OUTPUT_NAME ${project_name})
 

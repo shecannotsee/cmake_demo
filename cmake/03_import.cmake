041bc6dfb2c117f187ac422b35cca5db49fc539d
shecannotsee <shecannotsee@163.com>
2023-07-05 11:43:58 +0800

    [feat] user path

--- cmake/03_import.cmake
+++ cmake/03_import.cmake
@@ -1,12 +1,9 @@
-# Traverse list to add auto_third_party
-foreach(item IN LISTS 3rd_output)
-    # include
-    include_directories(${item}/include)
-    # lib
-    link_directories(${item}/lib)
 
-    message("Item: ${item}")
-endforeach()
+# include
+include_directories(${CMAKE_SOURCE_DIR}/third_party/googletest-install/include)
+# lib
+link_directories(${CMAKE_SOURCE_DIR}/third_party/googletest-install/lib)
+
 
 include_directories(${CMAKE_SOURCE_DIR}/src)
 include_directories(${CMAKE_SOURCE_DIR}/test)

5b3740a41997631b0cdd691b1c6ff2bb5f5a624f
shecannotsee <shecannotsee@163.com>
2023-07-05 11:45:09 +0800

    [feat] Add dependencies to build order

--- cmake/05_test.cmake
+++ cmake/05_test.cmake
@@ -2,8 +2,8 @@
 file(GLOB_RECURSE TEST_SRC "${CMAKE_SOURCE_DIR}/test/*")
 # list(REMOVE_ITEM SRC "${CMAKE_SOURCE_DIR}/src/TaskControl.cpp") # Exclude specific files
 
-set(test_name "${project_name}_test")
 
 # test target
 add_executable(${test_name} ${TEST_SRC} ${SRC})
+add_dependencies(${test_name} googletest)
 target_link_libraries(${test_name} ${lib})

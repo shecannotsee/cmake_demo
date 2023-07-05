14e9f62ffc11ce87064d00922e00499b2d6d7429
shecannotsee <shecannotsee@163.com>
2023-07-05 17:59:25 +0800

    [feat] Move to project to other place

--- cmake/00_config.cmake
+++ cmake/00_config.cmake
@@ -1,17 +1,1 @@
 
-set(project_name "cmake_demo")
-
-# [Release] or [Debug] or [MinSizeRel]
-set(compilation_options "Debug")
-
-# [library] or [executable]
-set(project_type "library")
-
-# target
-set(library_static_name "${project_name}_static")
-set(library_dynamic_name "${project_name}_dynamic")
-
-# test target
-set(test_name "${project_name}_test")
-
-
